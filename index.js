75b380e893e1ad694358a01d644890ffa62bb014
Mohan Kumar Kothala <33448410+mohankumar17@users.noreply.github.com>
2022-05-14 11:57:53 +0530

    ES6

---
+++ index.js
@@ -0,0 +1,48 @@
+/*
+Refactor the following function into a one-liner:
+const printName = ((name) => {
+         return “Hi” + name;
+    });
+
+*/
+
+const printName = ((name) => "Hi " + name);
+console.log(printName("Mohan"));
+
+/*
+Rewrite the following code using template literals
+const printBill = ((name, bill) => {
+        return “Hi “ + name + “, please pay: “ + bill;
+    });
+
+*/
+
+const printBill = ((name, bill) => {
+    return `Hi ${name}, please pay: ${bill}`;
+});
+
+console.log(printBill("Mohan", 1250));
+
+/*
+Modify the following code such that the object properties are destructured and logged.
+const person = {
+              name: “Noam Chomsky”,
+              age: 92
+            }
+           
+let name = person.name;
+let age = person.age;
+console.log(name);
+console.log(age);
+
+*/
+
+const person = {
+    name: "Noam Chomsky",
+    age: 92
+}
+
+const {name: name, age: age} = person;
+
+console.log(name);
+console.log(age);
